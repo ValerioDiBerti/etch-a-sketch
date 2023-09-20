f0008f86f84196617e81f38a8cc9e0bd9449d0e1
Valerio Di Berti <140324190+valeriodiberti@users.noreply.github.com>
2023-09-20 18:58:23 +0200

    clean up

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-const GRID_SIZE = 16;
+let gridSize = 16;
+let blocks;
 
 const grid = document.querySelector('.grid')
 
-for (i=0; i<SIZE*SIZE; i++) {
-    a = document.createElement('div');
-    a.setAttribute("class", "block");
-    a.style.width = `calc(100%/${GRID_SIZE})`;
-    a.style.height = `calc(100%/${GRID_SIZE})`;
-    grid.appendChild(a)
+makeGrid();
+
+function makeGrid() {
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.lastChild);
+    }
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        a = document.createElement('div');
+        a.setAttribute("class", "block");
+        a.style.width = `calc(100%/${gridSize})`;
+        a.style.height = `calc(100%/${gridSize})`;
+        grid.appendChild(a)
+        console.log(a);
+    }
+    blocks = document.querySelectorAll('.block');
+    blocks.forEach(block => {
+        block.addEventListener('mouseenter', () => {
+            block.style.background = 'red';
+        });
+        // block.addEventListener('mouseleave', () => {
+        //     block.style.background = 'white';
+        // });
+
+    })
 }
 
-const blocks = document.querySelectorAll('.block');
-blocks.forEach(block => {
-    
-    block.addEventListener('mouseenter', () => {
-        block.style.background = 'red';
-    });
-    // block.addEventListener('mouseleave', () => {
-    //     block.style.background = 'white';
-    // });
 
-})
+
 
 const eraseButton = document.querySelector('.erase');
-
 eraseButton.addEventListener('click', () => {
     blocks.forEach(block => {
         block.style.background = 'white';
     })
+})
+
+const gridSizeButton = document.querySelector('.grid-size');
+gridSizeButton.addEventListener('click', () => {
+    gridSize = parseInt(prompt('grid size?'))
+    makeGrid();
 })
